bcf31f24fc5174767f046302aad1ae7b86fb000f
Danny-Payami <75181334+Danny-Payami@users.noreply.github.com>
2020-11-30 18:26:17 +0330

    Update nowplaying.js

--- commands/nowplaying.js
+++ commands/nowplaying.js
@@ -1,37 +1,49 @@
 const createBar = require("string-progressbar");
 const { MessageEmbed } = require("discord.js");
+const { attentionembed } = require("../util/attentionembed");
 
 module.exports = {
-   name: "nowplaying",
+  name: "nowpLaying",
   aliases: ["np"],
   description: "Show now playing song",
-  execute(message) {
+  cooldown: 5,
+  execute(message, client) {
+       if(!message.guild) return;
+    //react with approve emoji
+ message.delete({timeout: 300}) 
     const queue = message.client.queue.get(message.guild.id);
-    if (!queue) return message.reply("There is nothing playing.").catch(console.error);
+   if (!queue) return attentionembed(message, "There is nothing playing.").catch(console.error);
 
     const song = queue.songs[0];
-    const seek = (queue.connection.dispatcher.streamTime - queue.connection.dispatcher.pausedTime) / 1000;
-    const left = song.duration - seek;
+    
+     let minutes = song.duration.split(":")[0];
+    let seconds = song.duration.split(":")[1];const seek = (queue.connection.dispatcher.streamTime - queue.connection.dispatcher.pausedTime) / 1000;
+    let ms = (Number(minutes)*60+Number(seconds));
+  
+ 
+    //define left duration
+    const left = ms - seek;
 
-    let nowPlaying = new MessageEmbed()
-      .setTitle("💫Now playing")
-      .setDescription(`**${song.title}\n${song.url}**`)
+
+   let nowPlaying = new MessageEmbed()
+      .setTitle("💫 Now playing")
+      .setDescription(`[**${song.title}**](${song.url})`)
+      .setThumbnail(song.thumbnail.url)
       .setColor("#ff0505")
       .setImage("https://cdn.discordapp.com/attachments/748630945640349756/776142404356276244/52-8ded5760a5e990dcb3ffbd7b73b5e74b090276dd.gif")
-      .setAuthor("Anndera Music", "https://cdn.discordapp.com/attachments/669950529090093105/776112872786231296/image0.png");
-    if (song.duration > 0) {
-      nowPlaying.addField(
-        "\u200b",
-        new Date(seek * 1000).toISOString().substr(11, 8) +
-          "[" +
-          createBar(song.duration == 0 ? seek : song.duration, seek, 20)[0] +
-          "]" +
-          (song.duration == 0 ? " ◉ LIVE" : new Date(song.duration * 1000).toISOString().substr(11, 8)),
-        false
-      );
-      nowPlaying.setFooter("Time Remaining: " + new Date(left * 1000).toISOString().substr(11, 8));
-    }
-
-    return message.channel.send(nowPlaying);
+      .setFooter(`${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }))
+      if(ms >= 10000) {
+        nowPlaying.addField("\u200b", "🔴 LIVE", false);
+        //send approve msg
+        return message.channel.send(nowPlaying);
+      }
+      //If its not a stream
+      if (ms > 0 && ms<10000) {
+        nowPlaying.addField("\u200b", 
+                            
+                            "**__[" + createBar((ms == 0 ? seek : ms), seek, 25, "▬", "⚪")[0] + "]__**\n" +  new Date(seek * 1000).toISOString().substr(11, 8) +  " / "  + (ms == 0 ? " ◉ LIVE" : new Date(ms * 1000).toISOString().substr(11, 8)), false );
+        //send approve msg
+        return message.channel.send(nowPlaying);
+      }
   }
 };
