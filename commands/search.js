07c199b4ddf5e364f98079b2e3d10c498c933583
Danny-Payami <75181334+Danny-Payami@users.noreply.github.com>
2020-11-30 18:22:14 +0330

    Update search.js

--- commands/search.js
+++ commands/search.js
@@ -23,7 +23,8 @@ async execute(message,args,client) {
      //get serverqueue
      const serverQueue = message.client.queue.get(message.guild.id);
     //react with approve emoji
-    message.react("✅").catch(console.error);
+    message.react("✅")
+      message.delete({timeout: 300}) .catch(console.error);
     //if the argslength is null return error
     if (!args.length)
       return attentionembed(message,`Usage: ${message.client.prefix}${module.exports.name} <Video Name>`)
