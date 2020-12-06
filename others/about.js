2855350dc372f3e2d345c7e20ead4af616d14759
Danny-Payami <75181334+Danny-Payami@users.noreply.github.com>
2020-12-07 03:09:20 +0330

    Update about.js

--- others/about.js
+++ others/about.js
@@ -22,7 +22,7 @@ module.exports = {
       .addField("> Bot Name :  ", `> **${client.user.tag}**`, true)
       .addField("> Bot ID :  ", `> **${client.user.id}**`, true)
       .addField("> Invite :  ", `> [Add Anndera For Server You.](https://discord.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=1140326208&scope=bot)`)
-      .addField("> Support :  ", `> [Kurd The Legend](https://discord.gg/ecz5fX8nv8)`)
+      .addField("> Support :  ", `> [Support Server Anndera](https://discord.gg/ecz5fX8nv8)`)
       .addField("> Bot Owner :  ", `> <@537241026839183371>`)
       .setImage("https://images-ext-2.discordapp.net/external/eSI8AEfgRqH0bP7U-pMdNNmyD91iEmr_sBpTRTVyTZo/https/media.discordapp.net/attachments/761322810144194560/765875969839726622/image3.gif?width=770&height=23")
       .setFooter(client.user.username, client.user.avatarURL())
